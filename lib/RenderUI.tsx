2e9c312a1bf276580fd3b9a6ae63642b849aef3e
Nirvik <das.nirv@gmail.com>
2025-04-26 07:55:43 -0700

    Import react

--- lib/RenderUI.tsx
+++ lib/RenderUI.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { defineCustomElements } from "@esri/calcite-components/loader";
 import { handlePlaceChange } from "./RenderUIUtils";
 
